c0e59539cdab9a7100a6b04bf0c6f1275551a79f
yngckzhk <yngckzhk@yahoo.co.jp>
2025-06-21 22:28:56 +0900

    tour

---
+++ data.js
@@ -0,0 +1,132 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-21",
+      "name": "駅前店側21",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2580.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8896502171303862,
+          "pitch": 0.2228073113630913,
+          "rotation": 11.780972450961727,
+          "target": "1-21"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-21",
+      "name": "和田21",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1383854592202063,
+          "pitch": 0.3274605945752267,
+          "rotation": 26.70353755551326,
+          "target": "0-21"
+        },
+        {
+          "yaw": -0.0922387909973903,
+          "pitch": 0.3792753455347171,
+          "rotation": 29.845130209103058,
+          "target": "2-21"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-21",
+      "name": "丸八21",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2584,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "駅前通り１",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
