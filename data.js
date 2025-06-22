3d6bac28ca159a15397b759d02fe99b0ae09d40a
yngckzhk <yngckzhk@yahoo.co.jp>
2025-06-22 11:29:54 +0900

    mega

--- data.js
+++ data.js
@@ -28,19 +28,32 @@ var APP_DATA = {
       ],
       "faceSize": 2584,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 1.480081394601024,
+        "pitch": -0.012367986103187789,
+        "fov": 1.0047556912135975
       },
       "linkHotspots": [
         {
-          "yaw": -2.5402433742430635,
-          "pitch": 0.32187794259330005,
-          "rotation": 0.7853981633974483,
+          "yaw": -2.132017536766984,
+          "pitch": 0.10722486308331369,
+          "rotation": 6.283185307179586,
           "target": "2-21k"
+        },
+        {
+          "yaw": 1.123455284475284,
+          "pitch": 0.07668818469755578,
+          "rotation": 0,
+          "target": "3-21"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 1.8431742288367179,
+          "pitch": 0.28469354974621197,
+          "title": "ひろや写真館",
+          "text": "おもちゃもあった"
+        }
+      ]
     },
     {
       "id": "1-eki21km",
@@ -108,22 +121,468 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.4084578370332199,
+        "pitch": -0.03316100607056427,
+        "fov": 1.4093427579990325
       },
       "linkHotspots": [
         {
-          "yaw": -0.36742264305966543,
-          "pitch": 0.3172532503081733,
-          "rotation": 10.995574287564278,
+          "yaw": -1.1828631900268807,
+          "pitch": 0.0746291573284168,
+          "rotation": 18.06415775814132,
           "target": "0-21"
         },
         {
-          "yaw": 0.7080040401553038,
-          "pitch": 0.3332828279147346,
-          "rotation": 1.5707963267948966,
+          "yaw": 1.8660787115160726,
+          "pitch": 0.09304071731944319,
+          "rotation": 7.0685834705770345,
           "target": "1-eki21km"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-21",
+      "name": "橋本パン屋21",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2579.5,
+      "initialViewParameters": {
+        "yaw": -0.8142109345389752,
+        "pitch": 0.008174829140385143,
+        "fov": 1.0469784443609496
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5824928621772631,
+          "pitch": 0.10755535531380112,
+          "rotation": 5.497787143782138,
+          "target": "0-21"
+        },
+        {
+          "yaw": 2.4573409113067264,
+          "pitch": 0.1016560951819585,
+          "rotation": 6.283185307179586,
+          "target": "4-21k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-21k",
+      "name": "宮永醤油21k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2587.5,
+      "initialViewParameters": {
+        "yaw": 2.179443303493569,
+        "pitch": 0.029159169107902727,
+        "fov": 1.0282090942527977
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3309602114048964,
+          "pitch": 0.14353279732654478,
+          "rotation": 0,
+          "target": "3-21"
+        },
+        {
+          "yaw": 2.806613490489216,
+          "pitch": 0.09230449346336478,
+          "rotation": 0,
+          "target": "5-21k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-21k",
+      "name": "白川肉屋21k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2595,
+      "initialViewParameters": {
+        "yaw": -3.1081317571350073,
+        "pitch": 0.032200824499280856,
+        "fov": 0.9417219900615081
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.32348758829694,
+          "pitch": 0.2714310020531112,
+          "rotation": 0.7853981633974483,
+          "target": "4-21k"
+        },
+        {
+          "yaw": 2.780324827436427,
+          "pitch": 0.08710426535167315,
+          "rotation": 0.7853981633974483,
+          "target": "7-21k"
+        },
+        {
+          "yaw": -0.028255135878964666,
+          "pitch": 0.10443940740197633,
+          "rotation": 0,
+          "target": "6-21k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-21k",
+      "name": "山森自転車横21k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2578.5,
+      "initialViewParameters": {
+        "yaw": 1.2425914698245393,
+        "pitch": 0.022455388769186513,
+        "fov": 0.999964254469045
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0306020757858017,
+          "pitch": 0.13067785625286454,
+          "rotation": 0.7853981633974483,
+          "target": "5-21k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-21k",
+      "name": "森岡21k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1003330603634982,
+        "pitch": -0.005092100034790548,
+        "fov": 1.1171328632289046
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.899431259152971,
+          "pitch": 0.10440568176823284,
+          "rotation": 6.283185307179586,
+          "target": "5-21k"
+        },
+        {
+          "yaw": -1.2192809720807567,
+          "pitch": 0.09796736264448924,
+          "rotation": 5.497787143782138,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "消防前令和",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2581,
+      "initialViewParameters": {
+        "yaw": 0.8169139643977026,
+        "pitch": -0.028109541042894648,
+        "fov": 0.9264330990391402
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4980623287485617,
+          "pitch": 0.1894512984787955,
+          "rotation": 7.0685834705770345,
+          "target": "7-21k"
+        },
+        {
+          "yaw": 0.7586792741329536,
+          "pitch": 0.06588326115453036,
+          "rotation": 6.283185307179586,
+          "target": "5-21k"
+        },
+        {
+          "yaw": -0.7763859888155604,
+          "pitch": 0.07687463894608726,
+          "rotation": 5.497787143782138,
+          "target": "9-21"
+        },
+        {
+          "yaw": 0.7462251069790238,
+          "pitch": 0.3654621929385229,
+          "rotation": 3.141592653589793,
+          "target": "11-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-21",
+      "name": "竹中21",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2596.5,
+      "initialViewParameters": {
+        "yaw": -1.1933171847263253,
+        "pitch": 0.027524741716661083,
+        "fov": 1.0169335930936614
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.433005882230699,
+          "pitch": 0.10833360694655703,
+          "rotation": 5.497787143782138,
+          "target": "10-21k"
+        },
+        {
+          "yaw": -2.644602527024695,
+          "pitch": 0.07110089100868677,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": 0.32584308768223735,
+          "pitch": 0.06721831811378642,
+          "rotation": 0.7853981633974483,
+          "target": "1-eki21km"
+        },
+        {
+          "yaw": 2.0448287063925425,
+          "pitch": 0.0749998793508535,
+          "rotation": 0,
+          "target": "3-21"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-21k",
+      "name": "橋川自転車21k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2574.5,
+      "initialViewParameters": {
+        "yaw": 0.02477305620222836,
+        "pitch": 0.018107213175071024,
+        "fov": 1.0773227335814397
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7338555885447047,
+          "pitch": 0.07871657910392571,
+          "rotation": 5.497787143782138,
+          "target": "9-21"
+        },
+        {
+          "yaw": 2.190612935040498,
+          "pitch": 0.12187658060033968,
+          "rotation": 0.7853981633974483,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-",
+      "name": "昭和へ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2581,
+      "initialViewParameters": {
+        "yaw": 0.7952566746853158,
+        "pitch": -0.022396639509837613,
+        "fov": 0.9400396154658409
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8003289570861742,
+          "pitch": 0.40818092286293606,
+          "rotation": 0,
+          "target": "8-"
         }
       ],
       "infoHotspots": []
